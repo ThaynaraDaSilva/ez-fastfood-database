cc106225d4e46a53d4c3f3a3296ff6f1b65ba655
Thaynara Silva <thaynara.silva@yaman.com.br>
2024-12-14 17:17:35 -0300

    inclusão de arquivo provider

--- vars.tf
+++ vars.tf
@@ -84,5 +84,21 @@ variable "rds_instance_name"{
   type        = string
   default     = "rds-dev-ezfastfood"
 }
+variable "aws_region" {
+  description = "Região AWS para os recursos"
+  type        = string
+  default     = "us-east-1" # Substitua pela região correta
+}
 
+variable "aws_access_key" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
 
